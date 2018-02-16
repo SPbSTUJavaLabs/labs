f251483d2fd1a6a7c00f43bb756cce8d8c8000e9
marsofandrew <marsofandrew@mail.ru>
2018-02-16 13:18:30 +0300

    make 3rd lab work

--- src/ru/spbstu/lab3/Main.java
+++ src/ru/spbstu/lab3/Main.java
@@ -1,51 +1,79 @@
 package ru.spbstu.lab3;
 
 public class Main {
-    static long count = 0;
 
-    public static void main(String[] args) {
-        Thread thread = new ThreadTask(10, 10);
-        Thread thread1 = new Thread(new Task(1000, 10));
+
+    public static void main(String[] args) throws InterruptedException {
+        Functions functions = new Functions();
+        Thread thread = new ThreadTask(functions, 10);
+        Thread thread1 = new Thread(new RunnableTask(functions, 10));
         thread.start();
         thread1.start();
     }
 
-    private static synchronized void add(int ammount) {
+
+}
+
+class RunnableTask implements Runnable {
+    private int iterations;
+    private Functions functions;
+
+    public RunnableTask(Functions functions, int iterations) {
+        this.iterations = iterations;
+        this.functions = functions;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < iterations; i++) {
+            functions.add1000();
+        }
+    }
+}
+
+class ThreadTask extends Thread {
+    private int iterations;
+    private Functions functions;
+
+    public ThreadTask(Functions functions, int iterations) {
+        this.iterations = iterations;
+        this.functions = functions;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < iterations; i++) {
+            functions.add10();
+        }
+    }
+}
+
+class Functions {
+    static int count = 0;
+    synchronized void add10() {
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        count += 10;
+        output();
+        notify();
+    }
+
+    synchronized void add1000() {
+        notify();
+        count += 1000;
+        output();
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
-
-    private static class ThreadTask extends Thread {
-        private Task task;
-
-        public ThreadTask(int plussAmmount, long iterations) {
-            task = new Task(plussAmmount, iterations);
-        }
-
-        @Override
-        public void run() {
-            task.run();
-        }
+    private synchronized void output() {
+        System.out.println(Thread.currentThread() + " count = " + count);
     }
-
-    private static class Task implements Runnable {
-        private int pluss;
-        private long iterations;
-
-        public Task(int plussAmmount, long iterations) {
-            pluss = plussAmmount;
-            this.iterations = iterations;
-        }
-
-        @Override
-        public void run() {
-            for (long i = 0; i < iterations; i++) {
-                synchronized (Task.class) {
-                    count += pluss;
-                    System.out.println(Thread.currentThread().getName() + " count = " + count);
-                }
-            }
-        }
-    }
-
 }
