06dea7a487dd5b6c0e5e32ad3bbb960bb6ca2592
marsofandrew <marsofandrew@mail.ru>
2018-02-16 13:45:14 +0300

    try to create SimpleRunnable

--- src/ru/spbstu/lab3/Main.java
+++ src/ru/spbstu/lab3/Main.java
@@ -3,15 +3,17 @@ package ru.spbstu.lab3;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        Functions functions = new Functions();
-        Thread thread = new ThreadTask(functions, 10);
-        Thread thread1 = new Thread(new RunnableTask(functions, 10));
+
+        Thread thread = new Thread(new SimpleTask(10, 10, true));
+        Thread thread1 = new Thread(new SimpleTask(1000, 10, false));
+
         thread.start();
         thread1.start();
     }
 
 }
 
+/*
 class RunnableTask implements Runnable {
     private int iterations;
     private Functions functions;
@@ -28,7 +30,8 @@ class RunnableTask implements Runnable {
         }
     }
 }
-
+*/
+/*
 class ThreadTask extends Thread {
     private int iterations;
     private Functions functions;
@@ -49,27 +52,42 @@ class ThreadTask extends Thread {
 class Functions {
     static int count = 0;
 
-    synchronized void add(int amount, boolean isFirst) {
-        waitOrNotify(isFirst);
-        add(amount);
-        waitOrNotify(!isFirst);
+}
+*/
+class SimpleTask implements Runnable {
+
+    private static long count = 0;
+    private static int plus;
+    private boolean isFirst;
+    private long iterations;
+
+    public SimpleTask(int plus, long iterations, boolean isFirst) {
+        this.plus = plus;
+        this.iterations = iterations;
+        this.isFirst = isFirst;
     }
 
-    private synchronized void waitOrNotify(boolean isFirst) {
-        if (isFirst) {
-           notify();
+    @Override
+    public void run() {
+        for (long i = 0; i < iterations; i++) {
+            synchronized (SimpleTask.class) {
+                waitOrNotify(isFirst);
+                count += plus;
+                System.out.println(Thread.currentThread() + " count = " + count);
+                waitOrNotify(!isFirst);
+            }
+        }
+    }
+
+    private synchronized void waitOrNotify(boolean isFirstA) {
+        if (isFirstA) {
+            notify();
         } else {
             try {
-                wait();
+              wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
-
-    private synchronized void add(int amount) {
-        count += amount;
-        System.out.println(Thread.currentThread() + " count = " + count);
-    }
-
 }
